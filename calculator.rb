732b8c494deca09c02facbe4b6a65f1f589a1d8c
Ruben Israel Olguin <rubenolguin01@gmail.com>
2020-07-20 03:07:44 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,15 @@
-# Add your variables here
+first_number = 4
+second_number = 2
+
+sum = first_number + second_number
+puts sum
+
+difference = first_number - second_number
+puts difference
+
+product = first_number * second_number
+puts product
+
+quotient = second_number % first_number
+puts quotient
+
